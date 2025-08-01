8bdd5410f8741ae4b98e6378019626e95a0fa5ff
gillianmaeconsolacion09-code <gillianmaeconsolacion09@gmail.com>
2025-08-01 17:22:11 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  typescript: {
-    // ignoreBuildErrors: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
